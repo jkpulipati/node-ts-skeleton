8105fc8d3e51cab3e73862a81d3cb52f1d3483fd
Jaya Krishna Pulipati <jaya.pulipati@gmail.com>
2021-02-09 19:06:36 +0530

    post call verification

--- src/api/controllers/ToursController.ts
+++ src/api/controllers/ToursController.ts
@@ -1,9 +1,11 @@
 import { Body, Get, JsonController, Post } from 'routing-controllers';
-import uuid from 'uuid';
+import { OpenAPI, ResponseSchema } from 'routing-controllers-openapi';
+// import uuid from 'uuid';
 import { Tours } from '../models/Tours';
 import { ToursService } from '../services/ToursService';
 
 @JsonController('/tours')
+@OpenAPI({ security: [{ basicAuth: [] }] })
 export class ToursController {
 
     constructor(
@@ -18,14 +20,13 @@ export class ToursController {
     }
 
     @Post()
+    @ResponseSchema(Tours)
     public create(@Body() body: Tours): Promise<Tours> {
-        const tour = new Tours();
-        tour.id = uuid.v1();
-        tour.name = body.name;
-        tour.summary = body.summary;
-        tour.description = body.description;
-        tour.price = body.price;
-        tour.maxGroupSize = body.maxGroupSize;
+        let tour = new Tours();
+        // tour.id = uuid.v1();
+        tour = {...tour, ...body};
+        console.log(tour);
+
         return this.toursService.create(tour);
     }
 }
